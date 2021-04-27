0c3eeb90b2fdbb7a87cd9b3ddf0d00f179aef493
Dgarraza <dgarraza@alumno.unlam.edu.ar>
2021-04-27 19:49:48 -0300

    primer commit 27/4

--- src/ar/edu/tarea/Cerradura.java
+++ src/ar/edu/tarea/Cerradura.java
@@ -2,4 +2,21 @@ package ar.edu.tarea;
 
 public class Cerradura {
 
+	private Boolean estadoCerrado;
+	private Integer codigo;
+
+	public Cerradura(Integer codigo) {
+		this.codigo = codigo;
+		this.estadoCerrado = Boolean.TRUE;
+	}
+
+
+	public void cerrarCerradura(Integer codigo) {
+		if (this.codigo == codigo) {
+			estadoCerrado = Boolean.TRUE;
+		}
+	}
+
+
+
 }
--- src/ar/edu/tarea/Puerta.java
+++ src/ar/edu/tarea/Puerta.java
@@ -2,5 +2,16 @@ package ar.edu.tarea;
 
 public class Puerta {
 
+	private Cerradura cerradura1;
+	private Cerradura cerradura2;
+	private Cerradura cerradura3;
+	private Boolean estadoPuerta;
+
+	public Puerta(Integer codigo1, Integer codigo2, Integer codigo3) {
+		Cerradura cerradura1 = new Cerradura(codigo1);
+		Cerradura cerradura2 = new Cerradura(codigo2);
+		Cerradura cerradura3 = new Cerradura(codigo3);
+	}
+
 	
 }
